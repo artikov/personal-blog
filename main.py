8f67c7d30e4bd03df96a87b1b6db5636041282f6
artikov <skrillem11@gmai.com>
2022-03-31 11:16:02 +0500

    bug fix

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ def add_new_post():
     return render_template("make-post.html", form=form, current_user=current_user)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=['POST', 'GET'])
 @admin_only
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
